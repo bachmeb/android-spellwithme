d8cbb9c39ec4ad214ebd4bf4bdb49e799a953c9b
bachmeb <bachmeb@mail.uc.edu>
2014-11-26 09:24:02 -0500

    connect to SetGetGo

---
+++ app/src/main/java/us/proximal/spellwithme/SetGetGoClient.java
@@ -0,0 +1,33 @@
+package us.proximal.spellwithme;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+/**
+ * Created by b on 11/26/14.
+ */
+public class SetGetGoClient {
+
+    private static final String BASE_URL = "http://randomword.setgetgo.com/get.php";
+
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        System.out.println("client.get");
+        //client.get(getAbsoluteUrl(url), params, responseHandler);
+        System.out.println(BASE_URL);
+
+        client.get(BASE_URL, params, responseHandler);
+
+        System.out.println("client got");
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+}
--- app/src/main/java/us/proximal/spellwithme/Welcome.java
+++ app/src/main/java/us/proximal/spellwithme/Welcome.java
@@ -12,8 +12,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
@@ -25,6 +29,10 @@ public class Welcome extends Activity {
 
     Button btnGotIt;
     Button btnMissedIt;
+    Button btnQueryBooks;
+    Button btnQueryHeroku;
+    Button btnQueryGoogle;
+
     TextView txtWord;
     EditText txtVocabulary;
     EditText txtScore;
@@ -32,6 +40,8 @@ public class Welcome extends Activity {
     WordServiceStub service;
 
     private static final String QUERY_URL = "http://openlibrary.org/search.json?q=";
+    private static final String HEROKU_URL = "http://proximal.herokuapp.com/api/v1/standards";
+
 
 
     @Override
@@ -46,6 +56,9 @@ public class Welcome extends Activity {
         service = new WordServiceStub();
 
         //instantiate the buttons in the onCreate method
+        btnQueryGoogle = (Button) findViewById(R.id.buttonQueryGoogle);
+        btnQueryBooks = (Button) findViewById(R.id.buttonQueryBooks);
+        btnQueryHeroku = (Button) findViewById(R.id.buttonQueryHeroku);
         btnGotIt = (Button) findViewById(R.id.buttonGotIt);
         btnMissedIt = (Button) findViewById(R.id.buttonMissedIt);
         txtWord = (TextView) findViewById(R.id.textWord);
@@ -61,8 +74,6 @@ public class Welcome extends Activity {
                 //get a new word
                 giveMeWord();
 
-
-
             } //end onClick
         } // end setOnClickListener
         ); //end new View.OnClickListener()
@@ -76,8 +87,26 @@ public class Welcome extends Activity {
                 //persist the score
 
                 //get a new word
-                queryBooks("apple");
+                //queryBooks("apple");
 
+                querySetGetGo();
+
+            } //end onClick
+        } // end setOnClickListener
+        ); //end new View.OnClickListener()
+
+
+        //attach onClick listeners to the button objects
+        btnQueryBooks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View currentView) {
+
+                //persist the score
+
+                //get a new word
+                //queryBooks("apple");
+
+                queryBooks("apple");
 
 
             } //end onClick
@@ -85,7 +114,127 @@ public class Welcome extends Activity {
         ); //end new View.OnClickListener()
 
 
+        //attach onClick listeners to the button objects
+        btnQueryHeroku.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View currentView) {
 
+                //queryHeroku("standards");
+                try {
+                    getPublicTimeline();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            } //end onClick
+        } // end setOnClickListener
+        ); //end new View.OnClickListener()
+
+        //attach onClick listeners to the button objects
+        btnQueryGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View currentView) {
+
+                queryGoogle();
+
+            } //end onClick
+        } // end setOnClickListener
+        ); //end new View.OnClickListener()
+
+    }
+
+    private void querySetGetGo() {
+        System.out.println("START querySetGetGo()");
+
+        //ProximalRestClient.get("statuses/public_timeline.json", null, new JsonHttpResponseHandler() {
+        SetGetGoClient.get("", null, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onFailure(int statusCode,
+                                  org.apache.http.Header[] headers,
+                                  java.lang.Throwable throwable,
+                                  org.json.JSONObject errorResponse){
+                System.out.println("fail 1");
+
+            }
+
+            @Override
+            public void onFailure(int statusCode,
+                                  org.apache.http.Header[] headers,
+                                  java.lang.Throwable throwable,
+                                  org.json.JSONArray errorResponse){
+                System.out.println("fail 2");
+
+            }
+
+            @Override
+            public void onFailure(int statusCode,
+                                  org.apache.http.Header[] headers,
+                                  java.lang.String responseString,
+                                  java.lang.Throwable throwable){
+                System.out.println("fail 3");
+
+            }
+            @Override
+            public final void onFailure(int statusCode,
+                                        org.apache.http.Header[] headers,
+                                        byte[] responseBytes,
+                                        java.lang.Throwable throwable){
+                System.out.println("Status code: " + statusCode);
+                System.out.println("Throwable: " + throwable.toString());
+                for(int i=0;i<headers.length;i++){
+                    System.out.println("Header: " + headers[i]);
+
+                }
+
+
+            }
+            @Override
+            public void onSuccess(int statusCode,
+                                  org.apache.http.Header[] headers,
+                                  java.lang.String responseString){
+                System.out.println("success 1");
+                System.out.println("Status code: " + statusCode);
+                for(int i=0;i<headers.length;i++){
+                    System.out.println("Header: " + headers[i]);
+
+                }
+                System.out.println("Response string: " + responseString);
+
+
+                txtWord.setText(responseString);
+
+
+            }
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // If the response is JSONObject instead of expected JSONArray
+                System.out.println("JSONObject success");
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
+
+                System.out.println("JSONArray success");
+
+                try {
+
+                    // Pull out the first event on the public timeline
+                    JSONObject firstEvent = (JSONObject) timeline.get(0);
+                    String tweetText = firstEvent.getString("text");
+
+                    // Do something with the response
+                    System.out.println(tweetText);
+
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+
+        });
+
+        System.out.println("END getPublicTimeLine()");
 
     }
 
@@ -114,6 +263,7 @@ public class Welcome extends Activity {
         String urlString = "";
         try {
             urlString = URLEncoder.encode(searchString, "UTF-8");
+
         } catch (UnsupportedEncodingException e) {
 
             // if this fails for some reason, let the user know why
@@ -153,6 +303,162 @@ public class Welcome extends Activity {
                 });
     }
 
+
+    private void queryHeroku(String what) {
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(HEROKU_URL, new AsyncHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+
+                System.out.println("HELLO");
+
+                System.out.println("Status code: " + statusCode);
+
+                for (int i=0;i<headers.length;i++){
+                    System.out.println("Headers: " + headers[i]);
+                }
+
+                for (int j=0;j<responseBody.length;j++){
+                    System.out.println("Response body: " + responseBody[j]);
+
+                }
+
+                System.out.println(responseBody.toString());
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable
+                    error)
+            {
+                error.printStackTrace(System.out);
+            }
+
+        });
+
+    }
+
+    private void queryGoogle(){
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("http://www.google.com", new AsyncHttpResponseHandler() {
+
+            @Override
+            public void onStart() {
+                // called before request is started
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                // called when response HTTP status is "200 OK"
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+            }
+
+            //@Override
+            public void onRetry(int retryNo) {
+                // called when request is retried
+            }
+        });
+
+    }
+
+        public void getPublicTimeline() throws JSONException {
+            System.out.println("START getPublicTimeLine()");
+
+            //ProximalRestClient.get("statuses/public_timeline.json", null, new JsonHttpResponseHandler() {
+            ProximalRestClient.get("", null, new JsonHttpResponseHandler() {
+
+                @Override
+                public void onFailure(int statusCode,
+                                      org.apache.http.Header[] headers,
+                                      java.lang.Throwable throwable,
+                                      org.json.JSONObject errorResponse){
+                    System.out.println("fail 1");
+
+                }
+
+                @Override
+                public void onFailure(int statusCode,
+                                      org.apache.http.Header[] headers,
+                                      java.lang.Throwable throwable,
+                                      org.json.JSONArray errorResponse){
+                    System.out.println("fail 2");
+
+                }
+
+                @Override
+                public void onFailure(int statusCode,
+                                      org.apache.http.Header[] headers,
+                                      java.lang.String responseString,
+                                      java.lang.Throwable throwable){
+                    System.out.println("fail 3");
+
+                }
+                @Override
+                public final void onFailure(int statusCode,
+                                            org.apache.http.Header[] headers,
+                                            byte[] responseBytes,
+                                            java.lang.Throwable throwable){
+                    System.out.println("Status code: " + statusCode);
+                    System.out.println("Throwable: " + throwable.toString());
+                    for(int i=0;i<headers.length;i++){
+                        System.out.println("Header: " + headers[i]);
+
+                    }
+
+
+                }
+                @Override
+                public void onSuccess(int statusCode,
+                                      org.apache.http.Header[] headers,
+                                      java.lang.String responseString){
+                    System.out.println("success 1");
+                    System.out.println("Status code: " + statusCode);
+                    for(int i=0;i<headers.length;i++){
+                        System.out.println("Header: " + headers[i]);
+
+                    }
+                    System.out.println("Response string: " + responseString);
+
+
+
+                }
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                    // If the response is JSONObject instead of expected JSONArray
+                    System.out.println("JSONObject success");
+                }
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
+
+                    System.out.println("JSONArray success");
+
+                    try {
+
+                        // Pull out the first event on the public timeline
+                        JSONObject firstEvent = (JSONObject) timeline.get(0);
+                        String tweetText = firstEvent.getString("text");
+
+                        // Do something with the response
+                        System.out.println(tweetText);
+
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                }
+
+            });
+
+            System.out.println("END getPublicTimeLine()");
+        }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
