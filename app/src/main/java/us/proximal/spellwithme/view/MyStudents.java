f06bab9880b078c8fddd6a8ff11ec9e311fc3678
bachmeb <bachmeb@mail.uc.edu>
2014-11-29 21:21:21 -0500

    add ListView to MyStudents activity

--- app/src/main/java/us/proximal/spellwithme/view/MyStudents.java
+++ app/src/main/java/us/proximal/spellwithme/view/MyStudents.java
@@ -1,36 +1,62 @@
 package us.proximal.spellwithme.view;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
 import us.proximal.spellwithme.R;
 
-public class MyStudents extends Activity {
+import static us.proximal.spellwithme.utility.ToastUtil.makeToast;
+
+public class MyStudents extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_students);
-    }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my_students, menu);
-        return true;
+        populateListView();
+
+        registerClickCallback();
+
+
+
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    private void populateListView(){
+        // Create list of items
+        String[] items = {"Blue", "Purple", "Red"};
+
+        // Build adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                R.layout.list_item_color,
+                items
+        );
+
+        // Configure the list view
+        ListView list = (ListView) findViewById(R.id.listMyStudents);
+        list.setAdapter(adapter);
+
     }
+
+    private void registerClickCallback(){
+
+        ListView list = (ListView) findViewById(R.id.listMyStudents);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                TextView textView = (TextView) view;
+                String message = "You clicked # " + i +
+                        " which is string: " + textView.getText().toString();
+                makeToast(MyStudents.this, message);
+            }
+        });
+    }
+
 }
